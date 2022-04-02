449d308496912617caeda09247605f1779b88fb8
Andrew McCall <amccall@qa.com>
2022-04-02 15:00:57 +0100

    Button Class

---
+++ app/src/main/java/com/andrew/sudoku/Button.java
@@ -0,0 +1,12 @@
+package com.andrew.sudoku;
+
+import android.graphics.Color;
+import android.graphics.Rect;
+
+public class Button {
+
+    public Button(Rect rect, String text, Color colour1, Color colour2){
+
+    }
+
+}
